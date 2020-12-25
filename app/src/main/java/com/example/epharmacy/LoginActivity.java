1c8c7408fcc613531909c0be2b8d69edcdf99a7d
dev-mrahsansaleem <ahsansaleem6389@gmail.com>
2020-12-25 12:05:23 +0500

    connect with firebase with multi user mode

---
+++ app/src/main/java/com/example/epharmacy/LoginActivity.java
@@ -0,0 +1,117 @@
+package com.example.epharmacy;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Objects;
+
+public class LoginActivity extends AppCompatActivity {
+    TextInputLayout txtEmail, txtPassword;
+    Button btnLogin, btnCreateAccount;
+
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+
+        Objects.requireNonNull(getSupportActionBar()).setTitle("Login");
+//        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);  // added for top bar arrow
+
+        txtEmail = findViewById(R.id.txtEmail);
+        txtPassword = findViewById(R.id.txtPassword);
+
+
+        btnLogin = findViewById(R.id.btnLogin);
+        btnCreateAccount = findViewById(R.id.btnCreateAccount);
+
+        btnLogin.setOnClickListener(v -> {
+            if (isFieldValid(txtEmail) && isFieldValid(txtPassword)) {
+                fAuth.signInWithEmailAndPassword(
+                        txtEmail.getEditText().getText().toString(),
+                        txtPassword.getEditText().getText().toString())
+                        .addOnSuccessListener(authResult -> {
+
+                            userNavigator(authResult.getUser().getUid());
+
+
+                        }).addOnFailureListener(e -> {
+                    Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                });
+
+            } else {
+                Toast.makeText(this, "Enter all fields", Toast.LENGTH_SHORT).show();
+            }
+        });
+        btnCreateAccount.setOnClickListener(v -> {
+            startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+        });
+
+
+    }
+
+    private void userNavigator(String uid) {
+
+        DocumentReference df = fStore.collection("Users").document(uid);
+        df.get().addOnSuccessListener(documentSnapshot -> {
+
+            if (documentSnapshot.get("userType").toString().equals("1")) {
+                //user
+                startActivity(new Intent(getApplicationContext(), UserActivity.class));
+                finish();
+            } else if (documentSnapshot.get("userType").toString().equals("2")) {
+                //pharmacist
+                startActivity(new Intent(getApplicationContext(), PharmacistActivity.class));
+                finish();
+            } else if (documentSnapshot.get("userType").toString().equals("3")) {
+                //admin
+                startActivity(new Intent(getApplicationContext(), AdminActivity.class));
+                finish();
+            }
+        }).addOnFailureListener(e -> {
+            Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+        });
+    }
+
+    private boolean isFieldValid(TextInputLayout txt) {
+        boolean valid = false;
+        if (txt.getEditText().getText().toString().isEmpty()) {
+            txt.setError("Empty Field");
+        } else {
+            valid = true;
+            txt.setError("");
+        }
+        return valid;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+            userNavigator(FirebaseAuth.getInstance().getUid());
+        }
+    }
+}
---
+++ app/src/main/java/com/example/epharmacy/RegisterActivity.java
@@ -0,0 +1,142 @@
+package com.example.epharmacy;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class RegisterActivity extends AppCompatActivity {
+    Button btnRegister;
+    TextInputLayout txtFirstName, txtLastName, txtEmail, txtUserName, txtPassword;
+    RadioGroup rgUserType;
+
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        Objects.requireNonNull(getSupportActionBar()).setTitle("Register");
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+
+        txtFirstName = findViewById(R.id.txtFirstName);
+        txtLastName = findViewById(R.id.txtLastName);
+        txtEmail = findViewById(R.id.txtRegEmail);
+        txtUserName = findViewById(R.id.txtUserName);
+        txtPassword = findViewById(R.id.txtPassword);
+
+        rgUserType = findViewById(R.id.rgUserType);
+
+        btnRegister = findViewById(R.id.btnRegister);
+
+        btnRegister.setOnClickListener(v -> {
+
+//        validate
+
+            if (isFieldValid(txtFirstName) && isFieldValid(txtLastName) &&
+                    isFieldValid(txtEmail) && isFieldValid(txtUserName) &&
+                    isFieldValid(txtPassword)) {
+//                Toast.makeText(this, "" + userType(rgUserType.getCheckedRadioButtonId()), Toast.LENGTH_SHORT).show();
+
+                fAuth.createUserWithEmailAndPassword(
+                        txtEmail.getEditText().getText().toString(),
+                        txtPassword.getEditText().getText().toString())
+                        .addOnSuccessListener(authResult -> {
+                            Toast.makeText(this, "Account Created", Toast.LENGTH_SHORT).show();
+
+
+                            DocumentReference df = fStore.collection("Users")
+                                    .document(authResult.getUser().getUid());
+                            Map<String, Object> userInfo = new HashMap<>();
+                            userInfo.put("firstName", txtFirstName.getEditText().getText().toString());
+                            userInfo.put("lastName", txtLastName.getEditText().getText().toString());
+                            userInfo.put("email", txtEmail.getEditText().getText().toString());
+                            userInfo.put("userName", txtUserName.getEditText().getText().toString());
+                            userInfo.put("userType", userType(rgUserType.getCheckedRadioButtonId()));
+
+                            df.set(userInfo).addOnSuccessListener(aVoid -> Toast.makeText(RegisterActivity.this, "Data Stored", Toast.LENGTH_SHORT).show());
+
+                            userNavigator(authResult.getUser().getUid());
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(RegisterActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+            } else {
+                Toast.makeText(this, "Enter all fields", Toast.LENGTH_SHORT).show();
+            }
+
+        });
+
+    }
+
+    private void userNavigator(String uid) {
+
+        DocumentReference df = fStore.collection("Users").document(uid);
+        df.get().addOnSuccessListener(documentSnapshot -> {
+
+            if (documentSnapshot.getString("userType").equals("1")) {
+                //user
+                startActivity(new Intent(getApplicationContext(), UserActivity.class));
+            } else if (documentSnapshot.getString("userType").equals("2")) {
+                //pharmacist
+                startActivity(new Intent(getApplicationContext(), PharmacistActivity.class));
+            } else if (documentSnapshot.getString("userType").equals("3")) {
+                //admin
+                startActivity(new Intent(getApplicationContext(), AdminActivity.class));
+            }
+            finish();
+        }).addOnFailureListener(e -> {
+            Toast.makeText(RegisterActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+        });
+    }
+
+    private boolean isFieldValid(TextInputLayout txt) {
+        boolean valid = false;
+        if (txt.getEditText().getText().toString().isEmpty()) {
+            txt.setError("Empty Field");
+        } else {
+            valid = true;
+            txt.setError("");
+        }
+        return valid;
+    }
+
+    private int userType(int id) {
+        if (id == R.id.rbUser) {
+            return 1;
+        } else if (id == R.id.rbPharmacist) {
+            return 2;
+        } else if (id == R.id.rbAdmin) {
+            return 3;
+        }
+        return -1;
+    }
+}
